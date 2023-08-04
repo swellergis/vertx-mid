c5b795085e015cc453b974a2e1182d19acf38e39
sweller <shaun.weller@gmail.com>
2023-08-04 07:44:59 -0600

    initial commit

---
+++ src/main/java/com/lumen/vertx/MainVerticle.java
@@ -0,0 +1,119 @@
+package com.lumen.vertx;
+
+import io.smallrye.mutiny.Uni;
+import io.smallrye.mutiny.vertx.core.AbstractVerticle;
+import io.vertx.core.DeploymentOptions;
+import io.vertx.core.json.JsonObject;
+import io.vertx.mutiny.core.Vertx;
+import io.vertx.mutiny.core.http.HttpServer;
+import io.vertx.mutiny.ext.web.Router;
+import io.vertx.mutiny.ext.web.RoutingContext;
+import io.vertx.mutiny.ext.web.handler.BodyHandler;
+import org.hibernate.reactive.mutiny.Mutiny;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.lumen.vertx.model.Customer;
+
+import javax.persistence.Persistence;
+import java.util.List;
+import java.util.Map;
+
+// tag::preamble[]
+public class MainVerticle extends AbstractVerticle {
+
+  private static final Logger logger = LoggerFactory.getLogger(MainVerticle.class);
+  private Mutiny.SessionFactory emf;  // <1>
+
+  @Override
+  public Uni<Void> asyncStart() {
+// end::preamble[]
+
+    // tag::hr-start[]
+    Uni<Void> startHibernate = Uni.createFrom().deferred(() -> {
+      // var pgPort = config().getInteger("pgPort", 5432);
+      var pgName = config().getString("pg.name");
+      var pgUri = config().getString("pg.uri");
+      var persistenceUnitName = config().getString("pu.name");
+
+      // var pgName = "mytest_azure";
+      // var pgUri = "192.168.57.35:5432";
+      // var persistenceUnitName = "atarcDS";
+
+      // var props = Map.of("javax.persistence.jdbc.url", "jdbc:postgresql://192.168.57.35:" + 5432 + pgName);  // <1>
+      // var props = Map.of("javax.persistence.jdbc.url", "jdbc:postgresql://localhost:" + pgPort + "/postgres");
+      var props = Map.of("javax.persistence.jdbc.url", "jdbc:postgresql://" + pgUri + "/" + pgName);
+      emf = Persistence
+        .createEntityManagerFactory(persistenceUnitName, props)
+        .unwrap(Mutiny.SessionFactory.class);
+
+      return Uni.createFrom().voidItem();
+    });
+
+    startHibernate = vertx.executeBlocking(startHibernate)  // <2>
+      .onItem().invoke(() -> logger.info("✅ Hibernate Reactive is ready"));
+    // end::hr-start[]
+
+    // tag::routing[]
+    Router router = Router.router(vertx);
+
+    BodyHandler bodyHandler = BodyHandler.create();
+    router.post().handler(bodyHandler::handle);
+
+    router.get("/customers").respond(this::listCustomers);
+    router.get("/customers/:id").respond(this::getCustomer);
+    router.post("/customers").respond(this::createCustomer);
+    // end::routing[]
+
+    // tag::async-start[]
+    Uni<HttpServer> startHttpServer = vertx.createHttpServer()
+      .requestHandler(router)
+      .listen(8080)
+      .onItem().invoke(() -> logger.info("✅ HTTP server listening on port 8080"));
+
+    return Uni.combine().all().unis(startHibernate, startHttpServer).discardItems();  // <1>
+    // end::async-start[]
+  }
+
+  // tag::crud-methods[]
+  private Uni<List<Customer>> listCustomers(RoutingContext ctx) {
+    return emf.withSession(session -> session
+      .createQuery("from Customer", Customer.class)
+      .getResultList());
+  }
+
+  private Uni<Customer> getCustomer(RoutingContext ctx) {
+    long id = Long.parseLong(ctx.pathParam("id"));
+    return emf.withSession(session -> session
+      .find(Customer.class, id))
+      .onItem().ifNull().continueWith(Customer::new);
+  }
+
+  private Uni<Customer> createCustomer(RoutingContext ctx) {
+    Customer customer = ctx.body().asPojo(Customer.class);
+    return emf.withSession(session -> session.
+      persist(customer)
+      .call(session::flush)
+      .replaceWith(customer));
+  }
+  // end::crud-methods[]
+
+  public static void main(String[] args) {
+    Vertx vertx = Vertx.vertx();
+
+    JsonObject config = new JsonObject();
+    config.put("pgName", "mytest_azure");
+    config.put("pgUri", "192.168.57.35:5432");
+    config.put("persistenceUnitName", "atarcDS");
+
+    DeploymentOptions options = new DeploymentOptions().setConfig(config); // <1>
+
+    vertx.deployVerticle(MainVerticle::new, options).subscribe().with(  // <2>
+      ok -> {
+        logger.info("✅ Deployment success");
+      },
+      err -> logger.error("🔥 Deployment failure", err));
+    // end::vertx-start[]
+  }
+
+}
---
+++ src/main/java/com/lumen/vertx/model/Customer.java
@@ -0,0 +1,100 @@
+package com.lumen.vertx.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GenerationType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import java.io.Serializable;
+
+@Entity
+public class Customer implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "emp_id")
+    private Long empId;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    private String dob;
+    private String city;
+
+    public Customer() {
+    }
+
+    public Customer(Long empId, String firstName, String lastName, String dob, String city) {
+        this.empId = empId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dob = dob;
+        this.city = city;
+    }
+
+    public Long getId() {
+        return id;
+    }
+    
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getEmpId() {
+        return this.empId;
+    }
+
+    public void setEmpId(Long empId) {
+        this.empId = empId;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getDob() {
+        return this.dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public String getCity() {
+        return this.city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", firstname='" + firstName + '\'' +
+                ", lastname='" + lastName + '\'' +
+                ", dob='" + dob + '\'' +
+                ", city='" + city + '\'' +
+                '}';
+    }
+
+}
