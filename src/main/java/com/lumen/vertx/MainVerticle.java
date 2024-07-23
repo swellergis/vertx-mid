d6ce5b5db2117a5cb0f047539b80599290853e83
swellergis <shaun.weller@gmail.com>
2024-07-23 17:00:48 -0600

    fix for query with where clause

--- src/main/java/com/lumen/vertx/MainVerticle.java
+++ src/main/java/com/lumen/vertx/MainVerticle.java
@@ -149,17 +149,17 @@ public class MainVerticle extends AbstractVerticle {
   }
 
   private Uni<String> getUserApiKey(RoutingContext ctx) {
-    String loginid = ctx.pathParam("loginid");
-    String queryString = String.format("from Users s where s.loginid = '%s'", loginid);
-    logger.info(String.format("queryString: %s", queryString));
+    String loginId = ctx.pathParam("loginid");
+    String queryString = "SELECT DISTINCT u FROM Users u WHERE u.loginId = :loginId";
 
     Uni<List<Users>> uniUsers = emf.withSession(session -> session
       .createQuery(queryString, Users.class)
+      .setParameter("loginId", loginId)
       .getResultList());
 
     return uniUsers.onItem()
       .transform(response -> response.iterator().next().getApiKey())
-      .onFailure().recoverWithItem("NotFoundException");
+      .onFailure().recoverWithItem("null");
   }
 
   private Uni<SsnLookup> createCustomer(RoutingContext ctx) {
